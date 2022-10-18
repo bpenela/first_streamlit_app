436eeecc96583f6dcc2c2f6d7df984e0291711e7
bpenela <112977152+bpenela@users.noreply.github.com>
2022-10-18 20:38:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,6 @@ st.header("The fruit load list contains:")
 st.dataframe(my_data_rows)
 
 add_my_fruit = st.text_input('What fruit would you like to add')
-if (add_my_fruit is not null):
+if (add_my_fruit is not None):
   my_cur.execute('insert into fruit_load_list values ('+ add_my_fruit +');')
   st.write('Thanks for adding ', add_my_fruit)
