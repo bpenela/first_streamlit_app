a410622172d4ad66a279aca153bafdafd87809d5
bpenela <112977152+bpenela@users.noreply.github.com>
2022-10-18 12:41:55 +0100

    Update streamlit_app.py
    Changed df index

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 #Variables
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = set_index('Fruit')
 
 st.title('Breakfast Favorites')
 
@@ -14,6 +15,6 @@ st.text('🥑🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Item picker
-st.multiselect('Pick some fruits:', list(my_fruit_list.set_index('Fruit'))
+st.multiselect('Pick some fruits:', list(my_fruit_list.index))
 # Available items
 st.dataframe(my_fruit_list)
